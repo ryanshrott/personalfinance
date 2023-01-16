9b7ae2e6430406d7ca6c9d1c243d63051b62a328
Ryan Shrott <ryanshrott@gmail.com>
2023-01-15 19:59:46 -0500

    commit

---
+++ app.py
@@ -0,0 +1,167 @@
+import streamlit as st
+import pandas as pd
+from st_aggrid import AgGrid
+from st_aggrid.grid_options_builder import GridOptionsBuilder
+from st_aggrid import AgGrid, JsCode, GridOptionsBuilder, GridUpdateMode
+from utils import *
+import math
+import requests
+
+
+st.set_page_config(page_title="💸 Portfolio Rebalancing App 💸", page_icon=":guardsman:", layout="wide")
+
+from streamlit.components.v1 import html
+
+button = """
+<script type="text/javascript" src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" data-name="bmc-button" data-slug="ryanshrott" data-color="#FFDD00" data-emoji="🍺"  data-font="Cookie" data-text="Buy me a beer" data-outline-color="#000000" data-font-color="#000000" data-coffee-color="#ffffff" ></script>
+"""
+
+html(button, height=70, width=220)
+
+st.markdown(
+    """
+    <style>
+        iframe[width="220"] {
+            position: fixed;
+            bottom: 60px;
+            right: 40px;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+st.title("💸 Portfolio Rebalancing App 💸")
+
+
+st.write("Enter the current prices, quantities, and desired allocations of your stocks and cash. Then click the Rebalance button to see the new quantities of each stock and the trading fees you will incur. At this time, only a single consistent currency is support. Do not mix currencies.")
+
+
+
+
+
+#gd.configure_column(field = '🔧',  
+#                    onCellClicked = js_add_row,
+#                    cellRenderer = cellRenderer_addButton,
+#                    lockPosition='left')
+#gd.configure_column(field = '🗑',
+#                    onCellClicked = js_delete_row,
+#                    cellRenderer = cellRenderer_deleteButton,
+#                    lockPosition='left')
+
+
+# This part for updating the Grid so that Streamlit doesnot rerun from whole
+cols = st.columns(2)
+with cols[0]:
+    n = st.number_input("Number of Stocks", value = 3, min_value = 1, max_value = 10, step = 1)
+with cols[1]:
+    transaction_fee = st.number_input("Flat Trading Fee ($) (per trade)", value = 6.95, min_value = 0.0, max_value = 100.0, step = 0.1)
+with st.form('Positions') as f:
+
+    st.header('Positions Inputs 🔖 (edit me)!')
+    df = pd.DataFrame({
+    'Stock': ['cash', 'XAW.TO', 'ZAG.TO', 'VCN.TO'] + ['Placeholder'] * (n - 3),
+    'Quantity': [6500] + [100, 400, 300] + [0] * (n - 3),
+    'Desired Allocation (%)': [1, 33, 33, 33] + [0] * (n - 3),
+    })
+    numeric_df = df[['Quantity', 'Desired Allocation (%)']]
+    numeric_df= numeric_df.applymap(lambda x: pd.to_numeric(x, errors='coerce'))
+
+    gd = GridOptionsBuilder.from_dataframe(df)
+    gd.configure_default_column(editable=True)
+    gd.configure_column(field = 'Desired Allocation (%)', width = 400)
+    #gd.configure_column(field = 'Price ($)', width = 160)
+    gd.configure_column(field = 'Stock', width = 150)
+    gridoptions = gd.build()
+    ag_grid  = AgGrid(df,
+                    gridOptions = gridoptions, 
+                    editable=True,
+                    allow_unsafe_jscode = True,
+                    theme = 'balham',
+                    height = 200,
+                    fit_columns_on_grid_load = True)
+
+    if st.form_submit_button("Rebalance", type="primary"):
+        df = ag_grid['data']
+        df = df[~((df['Quantity'] == 0) & (df['Desired Allocation (%)'] == 0))]
+        df['Price ($)'] = df['Stock'].apply(lambda x: get_price_vantage(x))
+        if df['Desired Allocation (%)'].sum() != 100:
+            st.error('Desired Allocation must sum to 100%')
+            st.stop()
+        prices = df['Price ($)'].values
+        names = df['Stock'].values
+        quantities = df['Quantity'].values
+        allocations = df['Desired Allocation (%)'].values
+        fee = transaction_fee * df[df.Stock != 'cash'].shape[0]
+
+        new_quantities = calculate_rebalanced_portfolio(prices, quantities, allocations, fee)
+        total_value = sum(float(prices[i]) * float(quantities[i]) for i in range(len(prices)))
+        st.write(f'Your portfolio value is ${total_value:,.2f}.')
+        st.write(f'Your total trading fees will be ${fee:,.2f}.')
+        df = update_table(df, new_quantities, transaction_fee)
+        cash = df[df['Stock'] == 'cash']['Dollar Value'].values[0]
+        df['price'] = df['Price ($)']
+        port_value_new = df["Desired Dollar Value"].sum() - fee
+
+        for c in ['Price ($)', 'Dollar Value', 'Desired Dollar Value']:
+            df[c] = df[c].apply(lambda x: "${:,.2f}".format(x))
+        for c in ['Desired Allocation (%)', 'Current Allocation (%)', 'Desired Quantity', 'Quantity']:
+            df[c] = df[c].apply(lambda x: "{:,.2f}".format(x))
+
+        st.write('Calculations table:')
+        st.write(df[[x for x in df.columns if x != 'price']])
+        df = df.sort_values('price', ascending=False)
+        #st.write(df)
+
+        def get_action_plan(df, allow_fractional_shares = True):
+            prices = df['Price ($)'].values
+            names = df['Stock'].values
+            new_quantities = [float(x.replace(',','')) for x in df['Desired Quantity'].values]
+            quantities = [float(x.replace(',','')) for x in df['Quantity'].values]
+            action_plan = []
+            sell_actions = []
+            buy_actions = []
+            for i in range(len(prices)):
+                if names[i] != 'cash':
+                    if new_quantities[i] > quantities[i]:
+                        a = new_quantities[i] - quantities[i]
+                        if allow_fractional_shares:
+                            action = f"Buy {a:.2f} shares of {names[i]}"
+                        else:
+                            action = f"Buy {math.floor(a)} shares of {names[i]}"
+                        buy_actions.append(action)
+                    elif new_quantities[i] < quantities[i]:
+                        a = quantities[i] - new_quantities[i]
+                        if allow_fractional_shares:
+                            action = f"Sell {a:.2f} shares of {names[i]}"
+                        else:
+                            action = f"Sell {math.ceil(a)} shares of {names[i]}"
+                        sell_actions.append(action)
+                    else:
+                        action = f"No action needed for {names[i]}"
+                        sell_actions.append(action)
+
+            action_plan = sell_actions + buy_actions
+            return action_plan
+        action_plan = get_action_plan(df)
+        st.write('💡 Pro tip: Sell first, then buy.')
+        st.markdown("### 📝 Action plan (fractional shares):")
+        for action in action_plan:
+            st.write("- " + action)
+        st.write('Your portfolio value after trading and fees will be ${:,.2f}'.format(port_value_new))
+
+        st.write('💡 Pro tip: If your trading platform does not support fractional shares, take the following conservative action plan:')
+        action_plan = get_action_plan(df, False)
+        st.markdown("### 📝 Action plan (no fractional shares):")
+        for action in action_plan:
+            st.write("- " + action)
+        st.balloons()
+
+
+
+show_disclaimer = st.button("Disclaimer")
+if show_disclaimer or st.session_state.get('show_disclaimer'):
+    st.session_state['show_disclaimer'] = True
+    st.markdown(legal_disclaimer(), unsafe_allow_html=True)
+else:
+    st.write("")
+
--- utils.py
+++ utils.py
@@ -6,12 +6,43 @@ import os
 import yfinance as yf
 
 dotenv.load_dotenv()
-# JavaScript function for the delete button
+
+
+def update_table(data, new_quantities, transaction_fee = 0.0):
+    price = data['Price ($)']
+    quantity = data.Quantity
+    total_value = sum(price[i] * quantity[i] for i in range(len(price)))
+    current_allocation = (price * quantity)/total_value*100
+    data["Current Allocation (%)"] = current_allocation
+    data['Dollar Value'] = price * quantity
+    data['Desired Quantity'] = new_quantities
+    data['Trading Fee'] = data['Stock'].apply(lambda x: transaction_fee if x != 'cash' else 0)
+    data['Desired Dollar Value'] = data['Desired Quantity'] * data['Price ($)']
+    return data[['Stock', 'Price ($)', 'Quantity',  'Desired Quantity', 'Dollar Value', 'Desired Dollar Value', 'Current Allocation (%)', 'Desired Allocation (%)', 'Trading Fee']]
+
+def calculate_rebalanced_portfolio(prices, quantities, allocations, fee):
+    total_value = sum(prices[i] * quantities[i] for i in range(len(prices))) - fee
+    new_quantities = [0] * len(prices)
+    for i in range(len(prices)):
+        new_quantities[i] = (allocations[i]/100.0* total_value) / prices[i]
+    return new_quantities
+
+
+js_add_row = JsCode("""
+function(e) {
+    let api = e.api;
+    let rowPos = e.rowIndex + 1; 
+    api.applyTransaction({addIndex: rowPos, add: [{}]})    
+};
+"""     
+)  
+
 js_delete_row = JsCode("""
 function(e) {
-    var api = e.api;
-    var rowIndex = e.rowIndex;
-    api.updateRowData({remove: [api.getDisplayedRowAtIndex(rowIndex).data]});
+let api = e.api;
+let selectedRow = api.getFocusedCell();
+let node = api.getRowNode(selectedRow.rowIndex);
+api.applyTransaction({ remove: [node.data] })
 };
 """)
 
@@ -51,16 +82,53 @@ cellRenderer_deleteButton = JsCode('''
     ''')
 
 
+# cellRenderer for the delete button
+cellRenderer_deleteButton = JsCode('''
+    class BtnCellRenderer {
+        init(params) {
+            this.params = params;
+            this.eGui = document.createElement('div');
+            this.eGui.innerHTML = `
+            <span>
+                <style>
+                .btn_delete {
+                    background-color: #f44336;
+                    border: 2px solid black;
+                    color: white;
+                    text-align: center;
+                    display: inline-block;
+                    font-size: 12px;
+                    font-weight: bold;
+                    height: 2em;
+                    width: 10em;
+                    border-radius: 12px;
+                    padding: 0px;
+                }
+                </style>
+                <button id='click-button' 
+                    class="btn_delete" 
+                    >&#x2718; Delete</button>
+            </span>
+        `;
+        }
+        getGui() {
+            return this.eGui;
+        }
+    };
+    ''')
+
+
+# JavaScript function 
+# api.applyTransaction({add: [{}]})   # This line would end row at the end always 
+# Finding row index is important to add row just after the selected index
 js_add_row = JsCode("""
 function(e) {
-    var api = e.api;
-    var rowPos = e.rowIndex + 1; 
-    var nextStock = String.fromCharCode(e.api.getDisplayedRowAtIndex(e.rowIndex).data.Stock.charCodeAt(e.api.getDisplayedRowAtIndex(e.rowIndex).data.Stock.length - 1) + 1);
-    api.applyTransaction({addIndex: rowPos, add: [{Stock: "Stock " + nextStock, Price: 0, Quantity: 0, "Allocation (%)": 0}]})    
+    let api = e.api;
+    let rowPos = e.rowIndex + 1; 
+    api.applyTransaction({addIndex: rowPos, add: [{}]})    
 };
 """     
-) 
-
+)  
 
 # cellRenderer with a button component.
 # Resources:
@@ -103,16 +171,19 @@ cellRenderer_addButton = JsCode('''
 def get_price_vantage(symbol):
     if symbol == 'CASH' or symbol=='cash' or symbol=='Cash':
         return 1
+    if 'Stock' in symbol:
+        return 0.0000001
     try:
         api_key = os.getenv('ALPHA_VANTAGE_API_KEY')
         data = requests.get(f'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={symbol}&apikey={api_key}').json()
         price = data["Global Quote"]["05. price"]
     except KeyError:
-        stock = yf.Ticker(symbol)
-        price = stock.info['regularMarketPrice']
-    except Exception:
-        st.warning(f'Error getting price for {symbol}')
-        price = None
+        try:
+            stock = yf.Ticker(symbol)
+            price = stock.info['regularMarketPrice']
+        except Exception:
+            st.error(f'Error getting price for {symbol}')
+            price = None
     return float(price)
 
 def get_price_yahoo(symbol):
@@ -123,3 +194,11 @@ def get_price_yahoo(symbol):
         st.warning(f'Error getting price for {symbol}')
         price = 0
     return price
+
+def legal_disclaimer():
+    disclaimer = """
+    <div style="font-size:9px;">
+    The information provided by this webapp is for general informational purposes only and is not intended to be a substitute for professional financial advice. The webapp is not responsible for any losses incurred as a result of using the information provided on the website. Users are solely responsible for ensuring that the information provided by the webapp is accurate and suitable for their individual needs. While we strive to provide accurate and up-to-date information, we cannot guarantee that all prices reflected on the webapp are 100% accurate and reflect current market conditions. The user acknowledges that the information provided on this webapp is subject to change without notice. By using this webapp, the user agrees to the terms of this disclaimer and releases the webapp and its owners from any and all liability.
+    </div>
+    """
+    return disclaimer
